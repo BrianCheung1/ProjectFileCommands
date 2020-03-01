e132a54eebaeaa46a993d4f789f95c38abf58f79
U-DESKTOP-L2D1G9R\ohitz <brian.cheugn1@baruchmail.cuny.edu>
2020-03-01 00:45:05 -0500

    Added comments to keep track of code

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(){
-    string Function;
-    string again;
+    string Function;    //changed from int to string to stop infinite loop
+    string again;       //Keep while loop going if user replies with "yes"
 
     do{
 
@@ -15,7 +15,7 @@ int main(){
             cout << "[3] Search a file " << endl;
             cout << "[9] Exit " << endl;
             cin >> Function;
-        } while(Function != "1" && Function != "2" && Function != "3" && Function != "9");
+        } while(Function != "1" && Function != "2" && Function != "3" && Function != "9"); //Only numbers 1,2,3,9 are allowed, other inputs result in question again
         
 
         if(Function == "1"){
@@ -25,7 +25,7 @@ int main(){
             string Filename;
             cin >> Filename;
 
-            ofstream outfile(Filename + ".txt");
+            ofstream outfile(Filename + ".txt"); //opens the file created by user
             outfile.close();
 
             while(Answer == "yes"){
@@ -41,7 +41,7 @@ int main(){
                 string Password;
                 cin >> Password;
 
-                outfile.open(Filename + ".txt", std::ios::app);
+                outfile.open(Filename + ".txt", std::ios::app); //opens file and writes at the end of the text file(ios:app)(append)
                 outfile << "Website: " << Sitename << endl;
                 outfile << "Username: " << Username << endl;
                 outfile << "Password: " << Password << endl << endl;
@@ -50,7 +50,7 @@ int main(){
                 cout << "Would you like to add more data? " << endl;
                 cin >> Answer;
 
-                while(Answer != "yes" && Answer != "no"){
+                while(Answer != "yes" && Answer != "no"){ //repeats question if user doesn't provide correct arugements yes/no
                     cout << "Must be yes or no." << endl;
                     cin >> Answer;
                 }
@@ -70,7 +70,7 @@ int main(){
             ifstream openfile(Filename + ".txt");
             if(openfile.is_open()){
                 if (openfile.is_open()){
-                    cout << openfile.rdbuf();
+                    cout << openfile.rdbuf(); //prints all contents of textfile
                 }
             }
             if(openfile.fail()){
@@ -94,9 +94,9 @@ int main(){
             while(openfile.good()){
                 getline(openfile, line);
                 pos = line.find(search);
-                if(pos != string::npos){
+                if(pos != string::npos){ //if postion doesnt equal no postion meaning it was found
                     cout << "Found" << endl;
-                    cout << "Line: " << line << endl;
+                    cout << "Line: " << line << endl; //prints entire line
                 }
             }
         }
@@ -109,7 +109,7 @@ int main(){
         do{
             cout << "Would you like to return to menu?" << endl;
             cin >> again;
-        }while(again != "yes" && again != "no");
+        }while(again != "yes" && again != "no"); //repeats question if user doesn't provide correct arugements yes/no
     
 
     }while(again == "yes");
